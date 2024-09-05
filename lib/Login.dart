79a4e66ba4adac6e9dab580512d93962f2c067fb
hassa <hassa@HASSANMUHAMMED>
2024-09-05 20:32:36 +0300

    make some things in login page

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,44 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: Stack(
+        children: [
+          Positioned(
+            right: -90,
+            top: 5,
+            child: Image(image: AssetImage('assets/images/img1.png'))
+            ),
+          Positioned(
+              top: 200,
+              left: 10,
+              child: Text('Welcome\nBack!',style: TextStyle(color: Colors.white , fontSize: 20 , fontWeight: FontWeight.bold),),)
+         ,
+         Positioned(
+            top: 280,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+            child: Column(children: [
+              Center(child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text('Login' ,style: TextStyle(color: Colors.indigo , fontSize: 30 ),),
+              ),)
+            ],),
+          ))
+        ,
+        
+        ],
+      ),
+    );
   }
 }
--- lib/SplashPage.dart
+++ lib/SplashPage.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      backgroundColor: Color.fromARGB(255, 53, 68, 155),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/login');
+        },
+        child: Stack(
+          children: [
+            Center(child: Image(image: AssetImage('assets/images/img1.png')),),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 50),
+                child: Text('Welcome' , style: TextStyle(color: Colors.white , fontSize: 20 , fontWeight: FontWeight.bold),)
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
